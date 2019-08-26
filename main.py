ba6db87c665c51514376c0a0118f23a1a49a7f95
Julio Moreira B. Barros <julio.barros@movile.com>
2019-08-26 20:59:44 -0300

    cloud dataset

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ if __name__ == '__main__':
         'Metro_Interstate_Traffic_Volume.csv',
         delimiter='\t', dtype=str
     )
-
     print(dataset)
+    dataset2 = []
+    for d in dataset:
+        print(d.split(','))
+        dataset2.append(d.split(',')[5])
+
+    print(set(dataset2))
